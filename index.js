4aea4a35c5b21e2b707cd5b390c5600953579fab
David Jaworski <jaws360@gmail.com>
2019-12-19 11:58:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+class Cat {
+  constructor(name, breed) {
+    this.name = name
+    this.sex = breed
+  }
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog {
+  constructor(name, breed) {
+    this.name = name
+    this.sex = breed
+  }
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Bird {
+  constructor(name, breed) {
+    this.name = name
+    this.sex = breed
+  }
+  speak() {
+    if (this.sex === "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
+  }
+}
